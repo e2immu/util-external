5be3108ac39b82a9d50d9244e05783cc643c218c
Bart Naudts <Bart.Naudts@codelaser.io>
2025-05-05 18:57:43 +0200

    add publishToMavenLocal

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,9 @@ tasks.register("test") {
 tasks.register("clean") {
     dependsOn(gradle.includedBuilds.map { it.task(":clean") })
 }
-tasks.register("publish") {
-    dependsOn(gradle.includedBuilds.map { it.task(":publish") })
+	tasks.register("publish") {
+	    dependsOn(gradle.includedBuilds.map { it.task(":publish") })
+}
+tasks.register("publishToMavenLocal") {
+    dependsOn(gradle.includedBuilds.map { it.task(":publishToMavenLocal") })
 }
