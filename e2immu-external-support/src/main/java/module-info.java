3de8adf555e30d054a8f8a68cfa86c1cab4ab953
Bart Naudts <Bart.Naudts@codelaser.io>
2025-06-30 09:56:37 +0200

    expose more packages in module-info.java

---
+++ e2immu-external-support/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module org.e2immu.util.external.support {
+    requires java.base;
+    exports org.e2immu.annotatedapi;
+    exports org.e2immu.annotation;
+    exports org.e2immu.annotation.eventual;
+    exports org.e2immu.annotation.type;
+    exports org.e2immu.annotation.method;
+    exports org.e2immu.annotation.rare;
+    exports org.e2immu.support;
+}
